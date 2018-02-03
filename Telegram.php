dbe751977bee7014a8794810050398029b0987c4
XavierOrejarena <7832637+XavierOrejarena@users.noreply.github.com>
2018-02-03 16:52:35 -0400

    Add files via upload

---
+++ Telegram.php
@@ -0,0 +1,24 @@
+#!/usr/bin/env php
+<?php
+ 
+$api = "bot542182830:AAEFUyXJeGUEiHGeu_SCs2Ej_IGC8olbgH4";
+ 
+$input = file_get_contents("https://api.telegram.org/bot542182830:AAEFUyXJeGUEiHGeu_SCs2Ej_IGC8olbgH4/getUpdates");
+$update = json_decode($input, true);
+ 
+$message = $update['message']['text'];
+$chatid = $update['message']['chat']['id'];
+ 
+function sendMessage($chatid, $text)
+{
+    global $api;
+    $url = "https://api.telegram.org/$api/sendMessage?chat_id=".$chatid."&text=".urlencode($text);
+    $get = file_get_contents($url);
+}
+ 
+if($message == "/start")
+{
+    sendMessage($chatid, "Iniciado");
+}
+ 
+?>
