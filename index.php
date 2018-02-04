a812a6e6970e9fd67c415f5d84a02681e01eaa92
Xavier Orejarena <xavierorejarena@Xaviers-MacBook-Pro.local>
2018-02-03 23:38:43 -0400

    make it better

--- index.php
+++ index.php
@@ -24,7 +24,7 @@ if($message == "/start")
     sendMessage($chatid, "Hola ".$name);
 }
 elseif ($message == "btc" or $message == "BTC") {
-	sendMessage($chatid, json_decode(file_get_contents("https://api.binance.com/api/v1/ticker/price?symbol=BTCUSDT"), true));
+	sendMessage($chatid, json_decode(file_get_contents("https://api.binance.com/api/v1/ticker/price?symbol=BTCUSDT")['price'], true));
 }
 else{
 	sendMessage($chatid, "Por ahora solo saludo, estoy ocupado tumbando al gobierno");
