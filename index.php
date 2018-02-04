742375065af13dc0f0e20e6574ab24151654f08d
Xavier Orejarena <xavierorejarena@Xaviers-MacBook-Pro.local>
2018-02-04 13:35:58 -0400

    make it better

--- index.php
+++ index.php
@@ -19,8 +19,9 @@ function sendMessage($chatid, $text)
 }
 
 if (true) {
-	$price = json_decode(file_get_contents("https://api.binance.com/api/v1/ticker/price?symbol=BTCUSDT"), true)['price'];
-	$sendMessage(149273661, $price);
+	$coin = "BTCUSDT";
+	$price = json_decode(file_get_contents("https://api.binance.com/api/v1/ticker/price?symbol=$coin"), true)['price'];
+	sendMessage($chatid, $price);
 }
 
  
