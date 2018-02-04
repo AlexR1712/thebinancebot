a3e888d28240aa70e0fb90b34223895d8777ae5c
Xavier Orejarena <xavierorejarena@Xaviers-MacBook-Pro.local>
2018-02-03 21:22:18 -0400

    make it better

--- index.php
+++ index.php
@@ -8,6 +8,9 @@ $update = json_decode($input, true);
 $message = $update['message']['text'];
 $chatid = $update['message']['chat']['id'];
 $name = $update['message']['from']['first_name'];
+$BINANCE_BTCUSDT = file_get_contents("https://api.binance.com/api/v1/ticker/price?symbol=BTCUSDT");
+$BINANCE_BTCUSDT = json_decode($BINANCE_BTCUSDT, true);
+$BTC = round($BINANCE_BTCUSDT['price']);
  
 function sendMessage($chatid, $text)
 {
@@ -19,6 +22,9 @@ function sendMessage($chatid, $text)
 if($message == "/start")
 {
     sendMessage($chatid, "Hola ".$name);
+}
+elseif ($message == "btc" or $message == "BTC") {
+	sendMessage($chatid, $BTC);
 }
 else{
 	sendMessage($chatid, "Por ahora solo saludo, estoy ocupado tumbando al gobierno");
