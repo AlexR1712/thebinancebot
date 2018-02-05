414232346605d9f777aa0f59f35ae43c20fd3c41
Xavier Orejarena <xavierorejarena@Xaviers-MacBook-Pro.local>
2018-02-05 04:23:59 -0400

    make it better

--- alarm.php
+++ alarm.php
@@ -18,6 +18,8 @@ while (true) {
 			$chatid = $alarmas[$i]['chatid'];
 			$seted_price = $alarmas[$i]['seted_price'];
 			$price = json_decode(file_get_contents("https://api.binance.com/api/v1/ticker/price?symbol=$coin"), true)['price'];
+			$seted_price = floatval($seted_price);
+			$price = floatval($price);
 			if ($price >= $seted_price) {
 				sendMessage($chatid, $coin." just reached the price of ".$seted_price);
 				//unset($alarmas[$i]);
