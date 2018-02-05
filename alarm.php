89c0e423c74bb5876f2b06ceb831f89fd0276c72
Xavier Orejarena <xavierorejarena@Xaviers-MacBook-Pro.local>
2018-02-05 03:41:01 -0400

    make it better

---
+++ alarm.php
@@ -0,0 +1,69 @@
+#!/usr/bin/env php
+<?php
+
+$api = "bot411509742:AAF0EfG_R-0mp3nL_KRTanuwDF79MiY2FRs";
+function sendMessage($chatid, $text)
+{
+    global $api;
+    $url = "https://api.telegram.org/$api/sendMessage?chat_id=".$chatid."&text=".urlencode($text);
+    $get = file_get_contents($url);
+}
+
+while (true) {
+	if (file_exists('alarmas.json')) {
+		$handle = fopen('alarmas.json', 'r');
+		$alarmas = json_decode(file_get_contents('alarmas.json'), true);
+
+		for ($i=0; $i < sizeof($alarmas); $i++) { 
+			$coin = substr($alarmas[$i]['coin'], 1, 10);
+			$chatid = $alarmas[$i]['chatid'];
+			$seted_price = $alarmas[$i]['seted_price'];
+			$price = json_decode(file_get_contents("https://api.binance.com/api/v1/ticker/price?symbol=$coin"), true)['price'];
+			if ($seted_price >= $price) {
+				sendMessage($chatid, $coin." ha superado el valor de ".$seted_price);
+				//unset($alarmas[$i]);
+				array_splice($alarmas, $i, 1);
+				$handle = fopen('alarmas.json', 'w');
+				file_put_contents('alarmas.json',  json_encode($alarmas));
+				fclose($handle);
+			}
+		}
+	}
+}
+
+
+
+
+
+
+
+
+// if (file_exists('alarmas.json')) {
+// 	$handle = fopen('alarmas.json', 'r');
+// 	$alarmas = json_decode(file_get_contents('alarmas.json'), true);
+// 	fclose($handle);
+// 	$alarm = array (
+//     'coin' => 'BTCUSDT', 
+//     'price' => '9100', 
+//     'chatid' => '12345'
+// 	);
+// 	$handle = fopen('alarmas.json', 'w');
+// 	$alarmas[] = $alarm;
+// 	file_put_contents('alarmas.json',  json_encode($alarmas));
+// 	fclose($handle);
+// }
+// else{
+// 	echo "No existe el archivo\n";
+// 	$coin = 'BTCUSDT';
+// 	$price = '9000';
+// 	$chatid = '123456';
+// 	$alarm[0] = array (
+//     'coin' => $coin, 
+//     'price' => $price, 
+//     'chatid' => $chatid);
+// 	$handle = fopen('alarmas.json', 'w');
+// 	file_put_contents('alarmas.json',  json_encode($alarm));
+// 	fclose($handle);
+// }
+
+?>
